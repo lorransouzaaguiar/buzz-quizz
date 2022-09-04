dcd2953a23879a26b6285908c1619b5b3074fc0c
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-09-04 15:14:31 -0300

    adiciona pagina info-basica dinamica

--- js/info-basica.js
+++ js/info-basica.js
@@ -1,8 +1,13 @@
-import { QuizzApi } from "./api.js"
 import { renderBody } from "./app.js"
 import { QuizzQuestionPage } from "./perguntas.js"
 
 export const BasicQuizzPage = () => {
+    const basicQuizzPage = Page()
+    renderBody(basicQuizzPage.render())
+    basicQuizzPage.formBasicQuizz()
+}
+
+const Page = () => {
 
     const render = () => {
         const page = document.createElement('div')
@@ -30,10 +35,7 @@ export const BasicQuizzPage = () => {
             const { respostaValidas, dados } = validarDadosQuizz(todosInputsValidos)
 
             if (respostaValidas) {
-                console.log('validado')
-                const quizzQuastionPage = QuizzQuestionPage(dados.numeroPerguntas, dados)
-                renderBody(quizzQuastionPage.render())
-                quizzQuastionPage.formQuestionQuizz()
+                QuizzQuestionPage(dados.numeroPerguntas, dados)
             } else {
                 alert('Preencha os dados corretamente!')
             }
@@ -42,64 +44,63 @@ export const BasicQuizzPage = () => {
         }
     }
 
-    return { render, formBasicQuizz }
-}
+    function validarDadosQuizz(todosInputsValidos) {
+
+        let inputs = document.querySelector('.criar-quizz')
+
+        let titulo = inputs.children[0].value
+
+        if (titulo.length >= 20 && titulo.length <= 65) {
+            todosInputsValidos.push(true)
+
+        } else {
+            todosInputsValidos.push(false)
+        }
+        let urlImagem = inputs.children[1].value
 
 
-export function validarDadosQuizz(todosInputsValidos) {
+        if (urlImagem.match('https?:\/\/.*\.(?:png|jpg)')) {
 
-    let inputs = document.querySelector('.criar-quizz')
+            todosInputsValidos.push(true)
+        } else {
 
-    let titulo = inputs.children[0].value
+            todosInputsValidos.push(false)
 
-    if (titulo.length >= 20 && titulo.length <= 65) {
-        todosInputsValidos.push(true)
+        }
 
-    } else {
-        todosInputsValidos.push(false)
-    }
-    let urlImagem = inputs.children[1].value
+        let numeroPerguntas = inputs.children[2].value
 
+        if (parseInt(numeroPerguntas) >= 3) {
 
-    if (urlImagem.match('https?:\/\/.*\.(?:png|jpg)')) {
+            todosInputsValidos.push(true)
 
-        todosInputsValidos.push(true)
-    } else {
+        } else {
+            todosInputsValidos.push(false)
 
-        todosInputsValidos.push(false)
+        }
 
-    }
+        let numeroNiveis = inputs.children[3].value
 
-    let numeroPerguntas = inputs.children[2].value
+        if (parseInt(numeroNiveis) >= 2) {
 
-    if (parseInt(numeroPerguntas) >= 3) {
+            todosInputsValidos.push(true)
 
-        todosInputsValidos.push(true)
+        } else {
+            todosInputsValidos.push(false)
 
-    } else {
-        todosInputsValidos.push(false)
+        }
 
-    }
+        let respostaValidas = todosInputsValidos.every((resposta) => resposta)
 
-    let numeroNiveis = inputs.children[3].value
-
-    if (parseInt(numeroNiveis) >= 2) {
-
-        todosInputsValidos.push(true)
-
-    } else {
-        todosInputsValidos.push(false)
-
-    }
-
-    let respostaValidas = todosInputsValidos.every((resposta) => resposta)
-
-    return {
-        respostaValidas, dados: {
-            titulo,
-            urlImagem,
-            numeroPerguntas,
-            numeroNiveis
+        return {
+            respostaValidas, dados: {
+                titulo,
+                urlImagem,
+                numeroPerguntas,
+                numeroNiveis
+            }
         }
     }
+
+    return { render, formBasicQuizz }
 }
