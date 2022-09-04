3e6b418b064d4e7cc28322bacddbb4a3279a65ee
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-09-04 15:14:54 -0300

    adiciona pagina niveis dinamica

--- js/niveis.js
+++ js/niveis.js
@@ -1,4 +1,16 @@
-export const QuizzLevelPage = (numberOfLevels, formQuestions) => {
+import { QuizzApi } from "./api.js"
+import { renderBody, userQuizzes } from "./app.js"
+import { SuccessQuizzPage } from "./sucesso.js"
+import { validations } from "./validations.js"
+
+export const QuizzLevelPage = (numberOfLevels, formQuestions, basicFormData) => {
+    const quizzLevelPage = Page(numberOfLevels, formQuestions, basicFormData)
+    renderBody(quizzLevelPage.render())
+    quizzLevelPage.formLevelQuizz()
+    quizzLevelPage.abrirNovoNivel()
+}
+
+const Page = (numberOfLevels, formQuestions, basicFormData) => {
 
     const render = () => {
         const page = document.createElement('div')
@@ -14,7 +26,7 @@ export const QuizzLevelPage = (numberOfLevels, formQuestions) => {
                     <img  src="./imagens/Vector.svg">
                 </div> 
                 <div class="criar-nivel escondido" id="criar-nivel" >
-                    <h3>Nível 2</h3>
+                    <h3>Nível ${i + 1}</h3>
                     <input placeholder="    Título do nível">  </input>
                     <input placeholder="    % acerto mínimo" id = "acerto-minimo" >  </input>
                     <input placeholder="    URL da imagem de nível">  </input>
@@ -26,139 +38,179 @@ export const QuizzLevelPage = (numberOfLevels, formQuestions) => {
 
         page.innerHTML = `
             <h2>Agora, decida os níveis</h2>
-            <div class="criar-nivel" id="criar-nivel">
-                <h3>Nível 1</h3>
-                <input placeholder="    Título do nível">  </input>
-                <input placeholder="    % acerto mínimo" id = "acerto-minimo">  </input>
-                <input placeholder="    URL da imagem de nível">  </input>
-                <input placeholder="    Descrição do nível"> </input>
-            </div>
             ${niveis.map(nivel => nivel)}
             <button class="finalizar">Finalizar Quizz</button>
         `
         return page
     }
 
+    const createQuizz = (formLevels, formQuestions, basicFormData) => {
+        const createQuizzQuestion = (title, image, questions, levels) => ({
+            title, image, questions, levels
+        })
+
+        const levels = formLevels.map(({ form }) => form)
+
+        const questions = formQuestions.map(({ form }) => form)
+        console.log(questions)
+
+        return createQuizzQuestion(
+            basicFormData.titulo, basicFormData.urlImagem, questions, levels)
+
+    }
+
+
     const formLevelQuizz = () => {
         let botaoNivel = document.querySelector('.finalizar')
+        const api = QuizzApi()
 
         botaoNivel.onclick = () => {
+            const formLevels = validarNiveisQuizz()
+            const isFormLevelValid = formLevels.every((({ respostaValidas }) => respostaValidas))
+
+            formLevels.forEach(({ respostaValidas }, index) => {
+                if (!respostaValidas)
+                    alert(`Preencha os dados corretamente do formulario ${index + 1}!`)
+            })
+
+            if (isFormLevelValid) {
+                const quizz = createQuizz(formLevels, formQuestions, basicFormData)
+
+                api.createQuizz(quizz).then(response => {
+                    const userQuizzesFromStorage = localStorage.getItem('id')
+                    console.log(userQuizzesFromStorage)
+
+                    if (userQuizzesFromStorage) {
+                        const userQuizzes = JSON.parse(userQuizzesFromStorage)
+                        userQuizzes.push(response.id)
+                        localStorage.setItem('id', JSON.stringify(userQuizzes))
+                        SuccessQuizzPage(basicFormData)
+                    } else {
+                        const userQuizzes = []
+                        userQuizzes.push(response.id)
+                        localStorage.setItem('id', JSON.stringify(userQuizzes))
+                    }
+
+                }).catch(console.log)
+            }
+        }
+    }
+
+    function validarNiveisQuizz() {
+        const inputsNivelForms = Object.values(document.querySelectorAll('.criar-nivel'))
+
+        return inputsNivelForms.map(inputsNivel => {
             const todosInputsNivelValidos = []
 
-            const { respostaValidas, dados } = validarNiveisQuizz(todosInputsNivelValidos)
+            const tituloNivel = inputsNivel.children[1].value
+            const acertoMinimo = inputsNivel.children[2].value
+            const urlImagemNivel = inputsNivel.children[3].value
+            const descriçaoNivel = inputsNivel.children[4].value
+            const verificarPorcentagem = Object.values(document.querySelectorAll('#acerto-minimo'))
+            const acerto = verificarPorcentagem.filter((input) => input.value == 0)
+            const val = validations()
 
-            console.log(formQuestions)
-            console.log(dados)
-            if (respostaValidas) {
-                console.log('validado')
+            console.log(inputsNivel)
 
-            } else {
-                alert('Preencha os dados corretamente!')
+            todosInputsNivelValidos.push(val.string().isGreaterOrEqualThen(tituloNivel, 10))
+            todosInputsNivelValidos.push(val.number().isBetweenArangeOf(parseInt(acertoMinimo), 0, 100))
+            todosInputsNivelValidos.push(val.string().isUrlImage(urlImagemNivel))
+            todosInputsNivelValidos.push(val.string().isGreaterOrEqualThen(descriçaoNivel, 30))
+            //todosInputsNivelValidos.push(val.string().isGreaterOrEqualThen(acerto, 0))
+
+            let respostaValidas = todosInputsNivelValidos.every((resposta) => resposta)
+
+            console.log(todosInputsNivelValidos)
+            console.log(respostaValidas)
+
+            return {
+                respostaValidas,
+                form: {
+                    title: tituloNivel,
+                    image: urlImagemNivel,
+                    minValue: acertoMinimo,
+                    text: descriçaoNivel,
+                }
             }
-            todosInputsNivelValidos.length = 0
-        }
+        })
+
     }
 
-    return { render, formLevelQuizz }
+    function abrirNovoNivel() {
+
+        let botaoNovoNivel = Object.values(document.querySelectorAll('.novo-nivel img'))
+        console.log(botaoNovoNivel)
+
+        botaoNovoNivel.forEach((botao, index) => {
+
+            botao.onclick = () => {
+                console.log(index)
+                let removerEscondido = Object.values(document.querySelectorAll('.escondido'))[index]
+                console.log(removerEscondido)
+
+                removerEscondido.classList.remove('escondido')
+
+                let removerNovoNivel = Object.values(document.querySelectorAll('.novo-nivel'))[index]
+
+                removerNovoNivel.classList.add('escondido')
+
+            }
+
+        })
+    }
+
+    return { render, formLevelQuizz, abrirNovoNivel }
 }
 
 
-export function validarNiveisQuizz(todosInputsNivelValidos) {
-    let inputsNivel = document.querySelector('.criar-nivel')
-    console.log(inputsNivel)
-
-    let tituloNivel = inputsNivel.children[1].value
-
-    if (tituloNivel.length >= 10) {
-
-        todosInputsNivelValidos.push(true)
-
-    } else {
-        todosInputsNivelValidos.push(false)
-    }
-
-    let acertoMinimo = inputsNivel.children[2].value
-
-    if (parseInt(acertoMinimo) >= 0 && parseInt(acertoMinimo) <= 100) {
-
-        todosInputsNivelValidos.push(true)
-
-    } else {
-
-        todosInputsNivelValidos.push(false)
-
-    }
-
-    let urlImagemNivel = inputsNivel.children[3].value
-
-    if (urlImagemNivel.match('https?:\/\/.*\.(?:png|jpg)')) {
-
-        todosInputsNivelValidos.push(true)
-    } else {
-
-        todosInputsNivelValidos.push(false)
-
-    }
-
-    let descriçaoNivel = inputsNivel.children[4].value
-
-    if (descriçaoNivel.length >= 30) {
-
-        todosInputsNivelValidos.push(true)
-
-    } else {
-
-        todosInputsNivelValidos.push(false)
-
-    }
-
-    let verificarPorcentagem = Object.values(document.querySelectorAll('#acerto-minimo'))
-
-    const acerto = verificarPorcentagem.filter((input) => input.value == 0)
-
-    if (acerto.length !== 0) {
-
-        console.log('VALIDADO')
-
-    } else {
-
-        console.log('deu ruim')
-    }
-
-    let respostaValidas = todosInputsNivelValidos.every((resposta) => resposta)
-
-    return {
-        respostaValidas, dados: {
-            tituloNivel,
-            acertoMinimo,
-            urlImagemNivel,
-            descriçaoNivel,
-            verificarPorcentagem
-        }
-    }
-}
-
-export function abrirNovoNivel() {
-
-    let botaoNovoNivel = Object.values(document.querySelectorAll('.novo-nivel img'))
-    console.log(botaoNovoNivel)
-
-    botaoNovoNivel.forEach((botao, index) => {
-
-        botao.onclick = () => {
-            console.log(index)
-            let removerEscondido = Object.values(document.querySelectorAll('.escondido'))[index]
-            console.log(removerEscondido)
 
 
+/* if (tituloNivel.length >= 10) {
 
-            removerEscondido.classList.remove('escondido')
+       todosInputsNivelValidos.push(true)
 
-            let removerNovoNivel = Object.values(document.querySelectorAll('.novo-nivel'))[index]
+   } else {
+       todosInputsNivelValidos.push(false)
+   } */
 
-            removerNovoNivel.classList.add('escondido')
 
-        }
+/*  if (parseInt(acertoMinimo) >= 0 && parseInt(acertoMinimo) <= 100) {
 
-    })
-}
+     todosInputsNivelValidos.push(true)
+
+ } else {
+
+     todosInputsNivelValidos.push(false)
+
+ } */
+
+
+/* if (urlImagemNivel.match('https?:\/\/.*\.(?:png|jpg)')) {
+
+    todosInputsNivelValidos.push(true)
+} else {
+
+    todosInputsNivelValidos.push(false)
+
+} */
+
+
+/*  if (descriçaoNivel.length >= 30) {
+
+     todosInputsNivelValidos.push(true)
+
+ } else {
+     todosInputsNivelValidos.push(false)
+
+ } */
+
+/* if (acerto.length !== 0) {
+
+    console.log('VALIDADO')
+
+} else {
+
+    console.log('deu ruim')
+} */
+
+/*  let respostaValidas = todosInputsNivelValidos.every((resposta) => resposta) */
