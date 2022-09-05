8cd3419e991e3fd272a311dcc1ca035e6414da7e
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-09-05 08:21:38 -0300

    adiciona funcionalidade da pagina do quizz (perguntas)

--- js/paginas/quizz.js
+++ js/paginas/quizz.js
@@ -3,12 +3,54 @@ import { renderBody } from "../app.js"
 export const QuizzPage = (quizzData) => {
     const quizzPage = Page(quizzData)
     renderBody(quizzPage.render())
+    quizzPage.chooseAnswer()
 }
 
 const Page = (quizzData) => {
+
     const render = () => {
 
         const page = document.createElement('div')
+
+        const renderAnswers = (answersData) => {
+            let answers = ''
+            const randomAnswers = answersData.sort(() => Math.random() - 0.5);
+            randomAnswers.forEach((answer, index) => {
+                answers += `
+                    <div class="quizz-answer" id="${JSON.stringify([index, answer.isCorrectAnswer])}">
+                        <div class="quizz-img-wrapper">
+                            <img src="${answer.image}" alt="">
+                        </div>
+                        <h3>${answer.text}</h3>
+                    </div>
+                `
+            })
+
+            return answers
+        }
+
+        const renderQuestions = (questionsData) => {
+
+            let quizz = ''
+
+            questionsData.forEach(question => {
+                const titleStyle = `
+                    background-color:${question.color === '#ffffff' ? '#DAD7D7' : question.color}";
+                    padding: 0 18px;
+                    color: var(--white1);
+                `
+
+                quizz += `
+                    <article class="wrapper-quizz">
+                        <h2 style="${titleStyle}>${question.title}</h2>
+                        <div class="group-image">
+                            ${renderAnswers(question.answers)}
+                        </div>    
+                    </article>`
+            });
+
+            return quizz
+        }
 
         page.innerHTML = `
             <div class="quizz-header">
@@ -23,43 +65,59 @@ const Page = (quizzData) => {
         return page
     }
 
-    const renderAnswers = (answersData) => {
-        let answers = ''
-        const randomAnswers = answersData.sort(() => Math.random() - 0.5);
-        randomAnswers.forEach(answer => {
-            answers += `
-                <div>
-                    <img src="${answer.image}" alt="">
-                    <h3>${answer.text}</h3>
-                </div>
-            `
+    const chooseAnswer = () => {
+        const quizzes = Object.values(document.querySelectorAll('.group-image'))
+
+        quizzes.forEach(quizzGroupEl => {
+            const quizzGroup = Array.from(quizzGroupEl.children)
+
+            quizzGroup.forEach((quizz) => {
+                const [id, isCorrectAnswer] = JSON.parse(quizz.getAttribute('id'))
+
+                const image = quizz.children[0]
+                const title = quizz.children[1]
+
+                quizz.onclick = () => {
+                    if (isCorrectAnswer) {
+                        image.style = ''
+                        title.style = `
+                            color: var(--green1);
+                        `
+                    } else {
+                        title.style = `color: var(--red3);`
+                    }
+
+                    console.log(id)
+                    const otherQuizzes = quizzGroup.filter(el => {
+                        const [otherId] = JSON.parse(el.getAttribute('id'))
+                        return otherId !== id
+                    })
+
+                    otherQuizzes.forEach(quizz => {
+                        const [_, isCorrectAnswer] = JSON.parse(quizz.getAttribute('id'))
+                        const image = quizz.children[0]
+                        const title = quizz.children[1]
+                        image.style = `opacity: 0.3;`
+
+                        if (isCorrectAnswer) {
+                            title.style = `
+                                color: var(--green1);`
+                        } else {
+                            title.style = `color: var(--red3);`
+
+                        }
+
+                        quizz.onclick = null
+                    })
+
+                    quizz.onclick = null
+                }
+            })
         })
 
-        return answers
+        /*  const */
+        /*  console.log(answersEl[0]) */
     }
 
-    const renderQuestions = (questionsData) => {
-
-        let quizz = ''
-
-        questionsData.forEach(question => {
-            const titleStyle = `
-                background-color:${question.color}";
-                padding: 0 18px;
-                color: var(--white1);
-            `
-
-            quizz += `
-                <article class="wrapper-quizz">
-                    <h2 style="${titleStyle}>${question.title}</h2>
-                    <div class="group-image">
-                        ${renderAnswers(question.answers)}
-                    </div>    
-                </article>`
-        });
-
-        return quizz
-    }
-
-    return { render }
+    return { render, chooseAnswer }
 }
